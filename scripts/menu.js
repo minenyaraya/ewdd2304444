44ef40cc50028010fb15b37d8134dce55d38db18
minenyaraya <minenyaraya@gmail.com>
2023-09-28 23:44:02 -0500

    index

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,7 +2,7 @@ const hambutton = document.querySelector("menu");
 const navegacion = document.querySelectorAll(".navegacion");
 // menuitems will be a Node List of the list items
 
-hamButtonbutton.addEventListener("click", () => {
+hambutton.addEventListener("click", () => {
   navegacion.classNavegacion.toggle("open");
   hamButtonbutton.classNavegacion.toggle("open");
 });
