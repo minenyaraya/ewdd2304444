257c9e9a97aa014404b0c9711c19b7d5749eae5c
Mineny <113462861+minenyaraya@users.noreply.github.com>
2023-10-07 12:53:49 -0500

    Delete Lesson03 directory

--- Lesson03/script/getDate.js
+++
@@ -1,8 +0,0 @@
-let date = new Date ();
-console.log(date);
-let year = date.getFullYear();
-console.log(year);
-document.querySelector(".year").textContent = year
-let currentdate = document.lastModified;
-console.log(currentdate);
-document.querySelector(".highlight").textContent=currentdate
